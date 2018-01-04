c74cdbeafbc9c505c3a18fad1ad47bc62dae1ae2
Maciej Billewicz <mbillewicz@cleeng.com>
2018-01-04 14:05:17 +0100

    Remove proxy method from customer class

--- src/Customer.php
+++ src/Customer.php
@@ -29,29 +29,23 @@ class Customer
         $frequentRenterPoints = 0;
         $result = "Rental Record for " . $this->getName() . "\n";
 
-        /** @var Rental $each */
-        foreach ($this->rentals as $each) {
+        /** @var Rental $rental */
+        foreach ($this->rentals as $rental) {
             $thisAmount = 0;
 
-            $thisAmount = $this->amountFor($each);
-
+            $thisAmount = $rental->getCharge();
             $frequentRenterPoints++;
-            if (($each->getMovie()->getPriceCode() == Movie::NEW_RELEASE)
-                && $each->getDaysRented() > 1
+            if (($rental->getMovie()->getPriceCode() == Movie::NEW_RELEASE)
+                && $rental->getDaysRented() > 1
             ) {
                 $frequentRenterPoints ++;
             }
-            $result .= $each->getMovie()->getTitle() . "\t" . $thisAmount . "\n";
+            $result .= $rental->getMovie()->getTitle() . "\t" . $thisAmount . "\n";
             $totalAmount += $thisAmount;
         }
         $result .= "Amount owned is " . $totalAmount . "\n";
         $result .= "You earned " . $frequentRenterPoints . " frequent renter points";
 
         return $result;
-    }
-
-    public function amountFor(Rental $aRental): float
-    {
-        return $aRental->getCharge();
     }
 }
